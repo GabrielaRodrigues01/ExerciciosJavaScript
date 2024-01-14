3926dd7b5dd0c3bfc7eeaa5f80df5f052f7d739e
Gabriela Rodrigues <78853618+GabrielaRodrigues01@users.noreply.github.com>
2024-01-14 13:32:25 -0300

    Inclusão de exercícios.

---
+++ 11-exercicio11.if/scripts.js
@@ -0,0 +1,15 @@
+let idade = 19;
+
+if(idade == 19){
+    console.log("A idade é = 19");
+}
+
+if(idade > 25){
+    console.log("A idade é maior que 25");
+}
+
+let nome = "Gabriela";
+
+if (nome == "Gabriela" && idade > 10){
+    console.log("Liberado!")
+}
