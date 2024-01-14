102d7ca5f91b8807b95cdd9f6ba5586506ed6fee
Gabriela Rodrigues <78853618+GabrielaRodrigues01@users.noreply.github.com>
2024-01-14 13:45:02 -0300

    Correção de exercício.

---
+++ 13-exercicicio13.else_if/scripts.js
@@ -0,0 +1,10 @@
+let nome = "Gabriela";
+let idade = 23;
+
+if(nome != undefined && nome == "Gabs"){
+    console.log("Nome está definido")
+} else if(nome == "Gabriela" && nome.length >5 && idade == 23) {
+    console.log("O nome é Gabriela");
+} else {
+    console.log("Não é Gabriela!")
+}
