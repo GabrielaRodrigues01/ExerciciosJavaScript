2620f2e78c0517bfab89db682dd3040f62f65f80
Gabriela Rodrigues <78853618+GabrielaRodrigues01@users.noreply.github.com>
2024-01-15 15:55:54 -0300

    Update scripts.js

--- 020-exercício20.decre/scripts.js
+++ 020-exercício20.decre/scripts.js
@@ -1,10 +1,10 @@
 let numero = 10;
 
-while(numero > 10){
+while(numero < 10){
     console.log(numero);
     numero -=1;
 }
 
-for(let i =10; i > 0; i--){
+for(let i = 10; i > 0; i--){
     console.log("O i é " - i);
 }
