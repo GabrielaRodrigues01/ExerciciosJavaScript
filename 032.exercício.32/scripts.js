cbd3c152dc70a55734ee1cada9183948a1eacbcd
Gabriela Rodrigues <78853618+GabrielaRodrigues01@users.noreply.github.com>
2024-02-01 23:09:56 -0300

    Inclusão de exercícios

---
+++ 032.exercício.32/scripts.js
@@ -0,0 +1,14 @@
+function imprimirNoConsole(){
+    console.log("Gabs!");
+}
+
+imprimirNoConsole();
+
+function imprimirNoConsole(num){
+    console.log("O número é:" + num);
+}
+
+imprimirNoConsole(2);
+imprimirNoConsole(4);
+imprimirNoConsole(6);
+imprimirNoConsole(8);
