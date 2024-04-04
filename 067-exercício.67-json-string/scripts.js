2a72a223422853015c52d17cbd5a71c835bd4dba
Gabriela Rodrigues <gabriela.oliveira@dnkinfotelecom.com.br>
2024-04-04 11:57:41 -0300

    Inclusão de scripts utilizando json.

---
+++ 067-exercício.67-json-string/scripts.js
@@ -0,0 +1,9 @@
+let pessoa = {
+    "nome": "Gabriela",
+    "idade": 23, 
+    "profissao": "programadora",
+    "hobbies": ["livros", "séries", "novelas"]
+}
+
+let pessoaTexto = JSON.stringify(pessoa);
+console.log(pessoaTexto);
